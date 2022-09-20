a6c313b2368110d2930d1b18355806056260fee0
harsh-choudhary-nature <harshchy2210@gmail.com>
2022-09-20 19:23:17 +0530

    commit

--- binary-search.cpp
+++ binary-search.cpp
@@ -1,12 +1,29 @@
 #include<stdio.h>
 #define notFound -1
+int BinarySearchRecursion(int* arr,int beg,int end,int size,int key){
+    // printf("Recursion\n");
+    if(beg>=end){
+        return notFound;
+    }
+    int mid=beg+(end-beg)/2;    //same as beg+end/2    
+    if(arr[mid]==key){
+        return mid;
+    }
+    if(arr[mid]>key){
+        end=mid;
+        return BinarySearchRecursion(arr,beg,end,size,key);
+    }else{
+        beg=mid+1;
+        return BinarySearchRecursion(arr,beg,end,size,key);
+    }
+}
 int BinarySearch(int* arr,int key,int size){
     //only for sorted arrays
     int beg=0,end=size-1;
     int mid;
     while(beg<end){
         // printf("Not found!\n");    
-        mid=(beg+end)/2;
+        mid=(end+beg)/2;
         if(arr[mid]==key){
             return mid+1; 
         }
@@ -21,8 +38,10 @@ int BinarySearch(int* arr,int key,int size){
 int main(){
     const int size=7;
     int arr[size]={1,2,3,4,5,6,7};
-    int search=-10;
-    int found=BinarySearch(arr,search,size);
+    int search=7;
+    int found=BinarySearchRecursion(arr,0,size-1,size,search);
+    int found1=BinarySearchRecursion(arr,0,size-1,size,search);
+    printf("Found at %dth position from left!\n",found1);
     if(found==notFound){
         printf("Not found!");
     }else{
--- ternary-search-algorithm.cpp
+++ ternary-search-algorithm.cpp
@@ -1,36 +1,47 @@
 #include<stdio.h>
-int ternarySearch(int arr[],int size,int key){
-    int beg=0,end=size-1;
+#define notFound -1;
+int ternarySearch(int* arr,int size,int key){
+    int div1,div2,beg,end;
+    beg=0;
+    end=size-1;
+    if(arr[end]==key){
+        return size-1;
+    }
     while(beg<end){
-        printf("isinfinite or not\n");
-        int div1=(beg+end)/3;       //2
-        int div2=2*(beg+end)/3;     //4
-        // printf(" %d and %f ",div,div2);
-        if(key==arr[div1]){
-            return div1+1;
-        }
+        div1=beg+(end-beg)/3;
+        div2=beg+2*(end-beg)/3;
         if(key<arr[div1]){
-            end=div1;               //0
+            end=div1;
             continue;
-        }else if(arr[div1]<key && key<arr[div2]){
+        }
+        if(key==arr[div1]){
+            return div1;
+        }
+        if(arr[div1]<key && key<arr[div2]){
             beg=div1+1;
             end=div2;
             continue;
         }
         if(key==arr[div2]){
-            return div2+1;
+            return div2;
         }
-        if(key>div2){
+        if(key>arr[div2]){
             beg=div2+1;
+            continue;
         }
     }
-    return -1;    
+    return notFound;
+
     
 }
 int main(){
     const int size=8;
     int arr[size]={1,2,3,4,5,6,7,8};
-    int key=9;
-    int position=ternarySearch(arr,size,key);
-    printf("%d",position);
+    int key=10;
+    while(key>-2){
+        int position=ternarySearch(arr,size,key);
+        printf("key=%d and position=%d\n",key,position);
+        key--;
+    }
+    return 0;
 }
